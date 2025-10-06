ced8d883f0358702d90ddff525125e6472ce4b6a
SpikeyDax <thaid@spu.edu>
2025-10-06 14:17:33 -0700

    Ver 3

--- main.cpp
+++ main.cpp
@@ -9,11 +9,15 @@ using std::endl;
 int main() {
     // Random number between 1 - 10
     srand(time(0));
-    int magic = 0;
+    int magic = 0, maxNumber, maxTries;
+    cout << "What is the max number to guess: ";
+    cin >> maxNumber;
+    cout << "What is the max tries: ";
+    cin >> maxTries;
     magic = (rand() % 10) + 1; // methods <> function
     int guess;
     bool won = false;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < maxTries; i++) {
         cout << "Guess #" << (i + 1) << "What is your guess: ";
     cin >> guess;
     if (magic == guess) {
